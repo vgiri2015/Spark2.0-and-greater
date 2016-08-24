4f7079457bed21a7c41ec670a7b1171477995d17
GiriR Varatharajan <giri.varatharajan@gmail.com>
2016-08-24 13:36:45 -0700

    Code Changes

--- src/main/scala/WordCountFileStreaming.scala
+++ src/main/scala/WordCountFileStreaming.scala
@@ -15,9 +15,14 @@ object WordCountFileStreaming {
     // stream to count words in new files created
     
     //Always listen for Latest directory and make sure it gets the files or records for every batches
-    val lines = ssc.textFileStream("/tmp/ssclisten/subfolder1/subfolder2")
+    val lines = ssc.textFileStream("/tmp/ssclisten/subfolder1/subfolder2") //Make Sure the File Name doesn't have '.' or ':' Hadoop path API does not support.
+    lines.foreachRDD({ rdd =>
+      val myFilesList = rdd.toDebugString
+      myFilesList.foreach(println)
+    })
     val words = lines.flatMap(_.split(" "))
     val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
+    wordCounts.count()
     wordCounts.print()
     ssc.start()
     ssc.awaitTermination()
