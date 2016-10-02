02e725509b2fdea36a55f5bf9148b38b034d8a9f
GiriR Varatharajan <giri.varatharajan@gmail.com>
2016-10-02 07:45:09 -0700

    New Files added

--- src/main/scala/ss/SSWindowedSocketWordCount.scala
+++ src/main/scala/ss/SSWindowedSocketWordCount.scala
@@ -11,11 +11,12 @@ import org.apache.spark.sql.functions._
 object SSWindowedSocketWordCount {
   def main(args: Array[String]): Unit = {
 
-    val windowDuration = 10
-    val slideDuration = 5
+    val windowDuration = s"10 seconds"
+    val slideDuration = s"5 seconds"
 
     val spark = SparkSession
       .builder
+      .master("local")
       .appName("StructuredNetworkWordCountWindowed")
       .getOrCreate()
 
@@ -29,10 +30,17 @@ object SSWindowedSocketWordCount {
       .option("includeTimestamp", true)
       .load()
 
+    //File Stream
+    //    val textssc = spark.readStream.text("/Users/vgiridatabricks/Downloads/ssc2.0/")
+    //    val wc = textssc.groupBy("word").count()
+
+
     // Split the lines into words, retaining timestamps
     val words = lines.as[(String, Timestamp)].flatMap(line =>
       line._1.split(" ").map(word => (word, line._2))
     ).toDF("word", "timestamp")
+
+    //    words.groupBy($"timestamp",window(windowDuration,slideDuration)).count()
 
     // Group the data by window and word and compute the count of each group
     val windowedCounts = words.groupBy(
